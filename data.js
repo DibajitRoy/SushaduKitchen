04ee41c788347b554bbfa00656b8536ae1a9c40e
DibajitRoy <dibajit.2003@gmail.com>
2026-03-20 02:16:31 +0600

    niewrecipes

---
+++ data.js
@@ -0,0 +1,12 @@
+var recipes = {
+    cheeseburger: {
+        title: "Cheeseburger",
+        image: "./pic/Cheeseburger.jpg",
+        difficulty: "Easy",
+        time: "20 Minutes",
+        ingredients: ["Bun", "Beef", "Cheese"],
+        steps: ["Cook", "Assemble", "Serve"]
+    },
+
+
+};
--- viewrecipe.js
+++ viewrecipe.js
@@ -2,43 +2,12 @@ function goBack(){
     window.history.back();
 }
 
-
-var recipes = {
-    cheeseburger: {
-        title: "Cheeseburger",
-        image: "./pic/Cheeseburger.jpg",
-        difficulty: "Easy",
-        time: "20 Minutes",
-        ingredients: ["Burger bun", "Beef patty", "Cheese", "Tomato"],
-        steps: ["Cook patty", "Toast bun", "Add cheese", "Serve"]
-    },
-
-    pizza: {
-        title: "Pizza",
-        image: "./pic/Pizza.avif",
-        difficulty: "Medium",
-        time: "30 Minutes",
-        ingredients: ["Dough", "Cheese", "Sauce"],
-        steps: ["Prepare base", "Add toppings", "Bake"]
-    },
-
-    creamypasta: {
-        title: "Creamy Pasta",
-        image: "./pic/Creamy Pasta.webp",
-        difficulty: "Easy",
-        time: "25 Minutes",
-        ingredients: ["Pasta", "Milk", "Cream"],
-        steps: ["Boil pasta", "Make sauce", "Mix"]
-    }
-};
-
-
 var params = new URLSearchParams(window.location.search);
 var recipeName = params.get("name");
 
 if(recipes[recipeName]){
 
-    var r = recipes[recipeName]; 
+    var r = recipes[recipeName];
 
     document.getElementById("recipeTitle").innerText = r.title;
     document.getElementById("recipeImage").src = r.image;
@@ -53,7 +22,6 @@ if(recipes[recipeName]){
         ingList.appendChild(li);
     }
 
-    
     var stepList = document.getElementById("recipeSteps");
 
     for(var i = 0; i < r.steps.length; i++){
