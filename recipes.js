5e71d343c582bffde044aa157fa7fa457d2d17c1
DibajitRoy <dibajit.2003@gmail.com>
2026-03-25 00:45:42 +0600

    categories

--- recipes.js
+++ recipes.js
@@ -4,44 +4,35 @@ function logout(){
 }
 
 function searchRecipe(){
-
     let input = document.getElementById("searchInput").value.toLowerCase();
     let cards = document.querySelectorAll(".card");
 
     cards.forEach(function(card){
-
         let title = card.querySelector("h2").innerText.toLowerCase();
 
         if(title.includes(input)){
             card.style.display = "inline-block";
-        }
-        else{
+        } else {
             card.style.display = "none";
         }
-
     });
 }
 
 function filterRecipe(level){
-
     let cards = document.querySelectorAll(".card");
 
     cards.forEach(function(card){
-
         let text = card.querySelector(".level").innerText;
 
         if(text == level){
             card.style.display = "inline-block";
-        }
-        else{
+        } else {
             card.style.display = "none";
         }
-
     });
 }
 
 function showAll(){
-
     let cards = document.querySelectorAll(".card");
 
     cards.forEach(function(card){
@@ -57,16 +48,13 @@ function showCategoryFromUrl(){
         let cards = document.querySelectorAll(".card");
 
         cards.forEach(function(card){
-
             let cat = card.getAttribute("data-category");
 
             if(cat == category){
                 card.style.display = "inline-block";
-            }
-            else{
+            } else {
                 card.style.display = "none";
             }
-
         });
     }
 }
